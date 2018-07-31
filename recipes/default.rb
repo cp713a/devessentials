c5d5e1b85347ca65cdbc12adf5dfac87d2f0befa
peter nguyen <peter.nguyen.h007@gmail.com>
2018-07-31 16:06:34 +0000

    adds apache recipe to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+#
+# This is my first attempt at using git.
+# This is my second attempt at using git.
+
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
